7a4e9805471326b23e39ae0f4dc8932e3e1604f7
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2023-05-31 12:57:25 +0300

    competed form style

--- src/Component/Form/Form.js
+++ src/Component/Form/Form.js
@@ -1,25 +1,58 @@
-import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import s from "./Form.module.css";
 
 const Form = () => {
   return (
     <div>
-      <h3>Form</h3>
-      <form>
-        <Box>
-          <Typography variant="h5">Full Name</Typography>
-          <Box>
+      <h3 className={s.title}>Form-Test</h3>
+      <form className={s.form} autoComplete="off">
+        <div>
+          <Typography variant="h5" component="h5" gutterBottom>
+            Full Name <span style={{ color: "red" }}>*</span>
+          </Typography>
+          <div className={s.smallWrapper}>
             <TextField required type="text" label="First Name" />
             <TextField required type="text" label="Last Name" />
-          </Box>
-        </Box>
-        <Box>
-          <Typography variant="h5">Address</Typography>
-          <TextField type="text" label="Street address" size={{ width: "100%" }} />
-          <TextField type="text" label="Street address L2" />
-        </Box>
-        <button type="submit">Submit</button>
+          </div>
+        </div>
+
+        <div>
+          <Typography variant="h5" component="h5" gutterBottom>
+            Address <span style={{ color: "red" }}>*</span>
+          </Typography>
+          <div className={s.adressWrapper}>
+            <TextField variant="outlined" type="text" label="Street address" fullWidth />
+            <TextField type="text" label="Street address L2" className={s.inputBig} />
+
+            <div className={s.smallWrapper}>
+              <TextField type="text" label="City" />
+              <TextField type="text" label="State / Province" />
+            </div>
+
+            <TextField type="text" label="Postal / Zip Code" />
+          </div>
+        </div>
+
+        <div className={s.smallWrapperBottom}>
+          <Typography variant="h5" component="h5" gutterBottom>
+            Phone number <span style={{ color: "red" }}>*</span>
+          </Typography>
+          <TextField type="text" label="Phone number" />
+        </div>
+
+        <div className={s.smallWrapperBottom}>
+          <Typography variant="h5" component="h5" gutterBottom>
+            Email <span style={{ color: "red" }}>*</span>
+          </Typography>
+          <TextField type="text" label="Email" />
+        </div>
+        <div className={s.buttonWrapp}>
+          <Button type="submit" color="success" variant="outlined" elementType="button" style={{ width: "200px" }}>
+            Submit
+          </Button>
+        </div>
       </form>
     </div>
   );
